85b4d940f8430e46a50170aca44a61c832aa35e6
TangyrenMidKing <65498450+TangyrenMidKing@users.noreply.github.com>
2021-07-29 18:09:19 -0400

    Add files via upload

--- codegen.c
+++ codegen.c
@@ -12,32 +12,31 @@ instruction *code;
 int code_index = 0;
 int jpc_index = 0;
 int jmp_index = 0;
-int sym_index = 0;
-int level = 0;
+int table_index = 0;
 lexeme *token;
 symbol *table;
 
 // prototypes
 void printcode();
 void program();
-void block();
-void const_decl();
-void const_decl_prime();
-int var_decl();
-int var_decl_prime();
-void proc_decl();
-void statement();
-void expression();
-void expression_prime();
-void condition();
-void term();
-void term_prime();
-void factor();
+void block(int level);
+void const_decl(int level);
+void const_decl_prime(int level);
+int var_decl(int level);
+int var_decl_prime(int level);
+void proc_decl(int level);
+void statement(int level);
+void expression(int level);
+void expression_prime(int level);
+void condition(int level);
+void term(int level);
+void term_prime(int level);
+void factor(int level);
 void unmark();
-void begin();
+void begin(int level);
 void next_token(int num);
 void emit(int op, int l , int m);
-int find(int type);
+int find(int level);
 
 instruction *generate_code(lexeme *tokens, symbol *symbols)
 {
@@ -51,52 +50,51 @@ instruction *generate_code(lexeme *tokens, symbol *symbols)
 
 void program()
 {
-	table[0].mark = 0;
+	unmark();
 	emit(7, 0, 0);
-	block();
+	block(0);
 	emit(9, 0, 3);
-	code[0].m = 3;
+	code[0].m = table[1].addr * 3;
 }
 
-void block()
+void block(int level)
 {
-	level++;
-	int proc_index = sym_index;
-	const_decl();
-	int num_var = var_decl();
-	proc_decl();
-	table[proc_index].level = code_index;
+	int proc_index = table_index;
+	const_decl(level);
+	int num_var = var_decl(level);
+	proc_decl(level);
+	table[proc_index].addr = code_index;
 	emit(6, 0, num_var + 3);
-	statement();
-	for (int i = 0; i <= sym_index; i++)
+	statement(level);
+	// mark all symbols declared here
+	for (int i = proc_index; i <= table_index; i++)
 		table[i].mark = 1;
-	level--;
 }
 
-void const_decl()
+void const_decl(int level)
 {
 	if (token->type == constsym)
 	{
 		next_token(4);
 		unmark();
-		const_decl_prime();
+		const_decl_prime(level);
 		next_token(1);
 	}
 }
 
-void const_decl_prime()
+void const_decl_prime(int level)
 {
 
 	if (token->type == commasym)
 	{
 		next_token(4);
 		unmark();
-		const_decl_prime();
+		const_decl_prime(level);
 		next_token(1);
 	}
 }
 
-int var_decl()
+int var_decl(int level)
 {
 
 	int num_var = 0;
@@ -104,90 +102,90 @@ int var_decl()
 	{
 		next_token(2);
 		unmark();
-		num_var = 1 + var_decl_prime();
+		num_var = 1 + var_decl_prime(level);
 		next_token(1);
 	}
 	return num_var;
 }
 
-int var_decl_prime()
+int var_decl_prime(int level)
 {
 	if (token->type == commasym)
 	{
 		next_token(2);
 		unmark();
-		return 1 + var_decl_prime();
+		return 1 + var_decl_prime(level);
 	}
 	return 0;
 }
 
-void proc_decl()
+void proc_decl(int level)
 {
 	if (token->type == procsym)
 	{
 		next_token(3);
 		unmark();
-		block();
+		block(1 + level);
 		next_token(1);
 		emit(2, 0, 0);
-		proc_decl();
+		proc_decl(level);
 	}
 }
 
-void statement()
+void statement(int level)
 {
 
 	/* const => 1 | var => 2 | proc => 3 */
 	if (token->type == identsym)
 	{
-		int index = find(2);
+		int index = find(level);
 		next_token(2);
-		expression();
+		expression(level);
 		emit(4, level - table[index].level, table[index].addr);
 	}
 	else if (token->type == callsym)
 	{
 		next_token(1);
-		int index = find(3);
+		int index = find(level);
 		next_token(1);
 		emit(5, level - table[index].level, table[index].level);
 	}
 	else if (token->type == writesym)
 	{
 		next_token(1);
-		expression();
+		expression(level);
 		emit(9,0,1);
 	}
 	else if (token->type == readsym)
 	{
 		next_token(1);
-		int index = find(2);
+		int index = find(level);
 		next_token(1);
 		emit(9,0,2);
-		emit(4,level - table[index].level, table[index].addr);
+		emit(4, level - table[index].level, table[index].addr);
 	}
 	else if (token->type == beginsym)
 	{
 		next_token(1);
-		statement();
-		begin();
+		statement(level);
+		begin(level);
 		next_token(1);
 	}
 	else if (token->type == ifsym)
 	{
 		next_token(1);
-		condition();
+		condition(level);
 		jpc_index = code_index;
 		emit(8,0,0);
 		next_token(1);
-		statement();
+		statement(level);
 		if (token->type == elsesym)
 		{
 			next_token(1);
 			jmp_index = code_index;
 			emit(7, 0, 0);
 			code[jpc_index].m = code_index * 3;
-			statement();
+			statement(level);
 			code[jmp_index].m = code_index * 3;
 		}
 		else
@@ -197,41 +195,41 @@ void statement()
 	{
 		next_token(1);
 		jmp_index = code_index;
-		condition();
+		condition(level);
 		next_token(1);
 		jpc_index = code_index;
 		emit(8, 0, 0);
-		statement();
+		statement(level);
 		emit(7, 0, jmp_index * 3);
 		code[jpc_index].m = code_index;
 	}
 }
 
-void begin()
+void begin(int level)
 {
 
 	if (token->type == semicolonsym)
 	{
 		next_token(1);
-		statement();
-		begin();
+		statement(level);
+		begin(level);
 	}
 }
 
-void condition()
+void condition(int level)
 {
 
 	if (token->type == oddsym)
 	{
 		next_token(1);
-		expression();
+		expression(level);
 		emit(2,0,6);
 	}
 	else
 	{
-		expression();
+		expression(level);
 		next_token(1);
-		expression();
+		expression(level);
 		switch(token->type)
 		{
 			 case eqlsym: emit(2,0,8); break;
@@ -244,85 +242,85 @@ void condition()
 	}
 }
 
-void 	expression()
+void 	expression(int level)
 {
 
 	if (token->type == plussym)
 	{
 		next_token(1);
-		term();
-		expression_prime();
+		term(level);
+		expression_prime(level);
 	}
 	else if (token->type == minussym)
 	{
 		next_token(1);
-		term();
+		term(level);
 		emit(2, 0, 1);
-		expression_prime();
+		expression_prime(level);
 	}
 	else
 	{
-		term();
-		expression_prime();
+		term(level);
+		expression_prime(level);
 	}
 }
 
-void expression_prime()
+void expression_prime(int level)
 {
 
 	if (token->type == plussym)
 	{
 		next_token(1);
-		term();
+		term(level);
 		emit(2, 0, 2);
-		expression_prime();
+		expression_prime(level);
 	}
 	else if (token->type == minussym)
 	{
 		next_token(1);
-		term();
+		term(level);
 		emit(2, 0, 3);
-		expression_prime();
+		expression_prime(level);
 	}
 }
 
-void term()
+void term(int level)
 {
-	factor();
-	term_prime();
+	factor(level);
+	term_prime(level);
 }
 
-void term_prime()
+void term_prime(int level)
 {
 	if (token->type == multsym)
 	{
 		next_token(1);
-		factor();
+		factor(level);
 		emit(2, 0, 4);
-		term_prime();
+		term_prime(level);
 	}
 	else if (token->type == slashsym)
 	{
 		next_token(1);
-		factor();
+		factor(level);
 		emit(2, 0, 5);
-		term_prime();
+		term_prime(level);
 	}
 	else if (token->type == modsym)
 	{
 		next_token(1);
-		factor();
+		factor(level);
 		emit(2, 0, 7);
-		term_prime();
+		term_prime(level);
 	}
 }
 
-void factor()
+void factor(int level)
 {
 
 	if(token->type == identsym)
 	{
-		int index = find(1);
+		int index = find(level);
 		if (table[index].kind == 2)
 			emit(3, level - table[index].level, table[index].addr);
 		if (table[index].kind == 1)
@@ -337,7 +335,7 @@ void factor()
 	else
 	{
 		next_token(1);
-		expression();
+		expression(level);
 		next_token(1);
 	}
 }
@@ -355,10 +353,10 @@ const => 1
 var => 2
 porc => 3
 */
-int find(int type)
+int find(int level)
 {
 	int min = 100, index = 0;
-	for (int i = sym_index; i >= 0; i--)
+	for (int i = table_index; i >= 0; i--)
 	{
 		if (strcmp(table[i].name,token->name) == 0)
 		{
@@ -375,7 +373,8 @@ void next_token(int num)
 
 void unmark()
 {
-	sym_index++;
+	table[table_index].mark = 0;
+	table_index++;
 }
 
 void printcode()
